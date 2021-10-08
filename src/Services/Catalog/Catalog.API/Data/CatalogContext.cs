89971ce6310ec0258c7aa43a26bdfe07f9ea133a
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2021-10-08 10:19:38 +0600

    Repository pattern introduced.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseString:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseString:CollectionName"));
+
+           Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+           CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
