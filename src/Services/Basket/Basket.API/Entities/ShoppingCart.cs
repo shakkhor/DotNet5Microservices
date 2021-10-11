80979f8cc4d7b71ed8f77f5f3cf555ef7b68d8cb
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2021-10-11 11:29:27 +0600

    Redis added for basket.API

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return ShoppingCartItems.Sum(x => x.Price);
+            }
+        }
+    }
+}
